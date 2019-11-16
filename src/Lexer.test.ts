b99aee1d2de8a7128de826e5eebf8b9a2ecfc4b4
Maksim Ryzhikov <rv.maksim@gmail.com>
2019-11-16 12:31:33 +0300

    chore: add eslint

--- src/Lexer.test.ts
+++ src/Lexer.test.ts
@@ -1,43 +1,43 @@
-import {Lexer} from './Lexer';
-import {Token} from './Token';
+import { Lexer } from './Lexer';
+import { Token } from './Token';
 
-describe('Lexer', function () {
-  it('should correctly parse empty string', function () {
+describe('Lexer', function() {
+  it('should correctly parse empty string', function() {
     expect(Lexer.parse('')).toEqual([]);
   });
 
-  it('should preserve spaces', function () {
+  it('should preserve spaces', function() {
     expect(Lexer.parse('  ')).toEqual([
       Token.WHITE_SPACE(' '),
       Token.WHITE_SPACE(' ')
     ]);
   });
 
-  it('should correctly parse colon symbol', function () {
+  it('should correctly parse colon symbol', function() {
     expect(Lexer.parse(':')).toEqual([Token.COLON()]);
   });
 
-  it('should correctly parse parens', function () {
+  it('should correctly parse parens', function() {
     expect(Lexer.parse('(')).toEqual([Token.LEFT_PAREN()]);
     expect(Lexer.parse(')')).toEqual([Token.RIGHT_PAREN()]);
   });
 
-  it('should correctly parse braces', function () {
+  it('should correctly parse braces', function() {
     expect(Lexer.parse('{')).toEqual([Token.LEFT_BRACE()]);
     expect(Lexer.parse('}')).toEqual([Token.RIGHT_BRACE()]);
   });
 
-  it('should correctly parse reserved keywords', function () {
+  it('should correctly parse reserved keywords', function() {
     expect(Lexer.parse('or')).toEqual([Token.OR()]);
     expect(Lexer.parse('and')).toEqual([Token.AND()]);
     expect(Lexer.parse('not')).toEqual([Token.NOT()]);
   });
 
-  it('should correctly parse value', function () {
+  it('should correctly parse value', function() {
     expect(Lexer.parse('foo')).toEqual([Token.VALUE('foo')]);
   });
 
-  it('should correctly parse value inside curly braces', function () {
+  it('should correctly parse value inside curly braces', function() {
     expect(Lexer.parse('{foo}')).toEqual([
       Token.LEFT_BRACE(),
       Token.VALUE('foo'),
@@ -52,7 +52,7 @@ describe('Lexer', function () {
     ]);
   });
 
-  it('should correctly parse a query with field name and field value', function () {
+  it('should correctly parse a query with field name and field value', function() {
     expect(Lexer.parse('foo: bar')).toEqual([
       Token.VALUE('foo'),
       Token.COLON(),
@@ -61,13 +61,13 @@ describe('Lexer', function () {
     ]);
   });
 
-  it('should not throw exception if do not know symbol', function () {
+  it('should not throw exception if do not know symbol', function() {
     expect(Lexer.parse('.')).toEqual([
       Token.VALUE('.')
     ]);
   });
 
-  it('should correctly parse incomplete string', function () {
+  it('should correctly parse incomplete string', function() {
     expect(Lexer.parse('"foo')).toEqual([
       Token.VALUE('"'),
       Token.VALUE('foo')
